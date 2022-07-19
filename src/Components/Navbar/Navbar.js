738684f252f508bd31cd41c9d602c4381e41af48
MacarioIG <igarzabalmacario@gmail.com>
2022-07-19 00:03:35 -0300

    Primer entraga proyecto final

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,10 +4,10 @@ import CartWidget from '../cartWidget/CartWidget';
 import { NavLink } from 'react-router-dom';
 
 //ICONS
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, faTv } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faMobileScreenButton } from '@fortawesome/free-solid-svg-icons'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
 
 
 
@@ -19,7 +19,7 @@ function Navbar() {
             <NavLink className="img"to = "/"><img src='/images/logoDarkMode.png' alt='' /></NavLink>
             <div className='Nav__container'>
                 <NavLink to="/categorys/phones"><NavbarButton textButton="SmartPhones" icon={faMobileScreenButton} /></NavLink>
-                <NavLink to="/categorys/tv"><NavbarButton textButton="Suscribe" icon={faPenToSquare} /></NavLink>
+                <NavLink to="/categorys/tv"><NavbarButton textButton="Smart Tv's" icon={faTv} /></NavLink>
                 <NavbarButton textButton="My account" icon={faUser} />
                 <CartWidget icon={faCartShopping} />
             </div>
